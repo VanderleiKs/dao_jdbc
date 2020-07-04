16e63f6a68826c53011dcfc9e3e94aa455b4a942
Vanderlei K Silva <vanderleiksilva@gmail.com>
2020-07-04 16:43:45 -0300

    Implementation method findByDepartment

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -89,6 +92,44 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = conn.prepareStatement(
+				"Select seller.*, department.Name as DepName "
+				+ "FROM seller INNER JOIN department "
+				+ "ON seller.DepartmentId = department.Id "
+				+ "WHERE DepartmentId = ? "
+				+ "ORDER BY Name");
+			ps.setInt(1, department.getId());
+			rs = ps.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> departId = new HashMap<>();
+			
+			while(rs.next()) {
+				Department dep = departId.get(rs.getInt("DepartmentId"));
+		
+				if(dep == null) {
+					dep = getDepartament(rs);
+					departId.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller seller = getSeller(rs, dep);
+				list.add(seller);
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeResusltSet(rs);
+			DB.closeStatement(ps);
+		}
 	}
 
 }
--- src/view/App.java
+++ src/view/App.java
@@ -1,7 +1,10 @@
 package view;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
@@ -13,10 +16,17 @@ public class App {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		
-		Seller seller = sellerDao.findById(2);
+		Seller seller = sellerDao.findById(3);
+		
+		List<Seller> seller2 = sellerDao.findByDepartment(new Department(1, null));
 		
 		System.out.println("=== TEST 1: FindById ===");
 		System.out.println(seller);
+		
+		System.out.println("\n=== TEST 2: FindByDepartment ===");
+		for(Seller obj : seller2) {
+		System.out.println(obj);
+		}
 	}
 
 }

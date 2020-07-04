fa061838561b9a098d785820234afa63d144c8ff
Vanderlei K Silva <vanderleiksilva@gmail.com>
2020-07-04 11:52:41 -0300

    Create class DaoFactory and SellerDaoJDBC

--- src/view/App.java
+++ src/view/App.java
@@ -2,6 +2,8 @@ package view;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class App {
 		Department dep = new Department(1, "Deposit");
 		
 		Seller seller = new Seller(23, "Marcelo", "marcelo@gmail.com", new Date(), 3400.00, dep);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
